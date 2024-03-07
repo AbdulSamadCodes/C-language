e80d5e8575f7d3196294590f96d205bc739fb984
Samaadcodes <samadcom797@gmail.com>
2024-03-07 22:27:01 +0500

    Contol instrunctions added

---
+++ Conditionals instructions.c
@@ -0,0 +1,88 @@
+# include <stdio.h>
+
+
+int main()
+{
+
+    //Write a program whether user is eligible to vote
+   
+  //  int age;
+  //  scanf("%d" , age);
+
+  //  if(age >= 18)
+  //  {
+  //    printf("You are  eligible to vote");
+  //  }
+  //  else
+  //  {
+  //    printf("You are  not eligible to vote");
+  //  }
+
+   //Write a program to check whether number  is 1 , 2 , 3 and print.
+
+  //  int num;
+  //  printf("Enter your number");
+  //  scanf("%d" , &num);
+
+  //  if(num == 1){printf("Number is 1");}
+
+  //  else if(num == 2) {printf("Number is 2");}
+
+  //  else if(num == 3) {printf("Number is 3");}
+
+  //  else{printf("Number is not 1 , 2 or 3");}
+
+   
+   //In C language non-zero value is considered true
+
+   // == is assignmet opertor
+   // >= is less than or equal to
+  // <= is greater than or equal to
+
+
+  //Logical opeartors in C are
+
+  //--&&  is  logical  'And' operator
+  //--||  is logical 'Or' operator
+  //--! is logical 'Not' operator
+
+  //=====Ternary opeartor===========
+
+  //Ternary operator are used for single line if else statements
+
+  // int age = 20;
+
+  // age > 10 ? printf("Age is greater than 10") : printf("Age is less than 10");
+
+
+  //=============Switch case control instrunctions=============
+
+    // int rating;
+    // printf("Enter your rating:");
+    // scanf("%d" , &rating);
+    
+    // switch(rating)
+    // {
+    //     case 1:
+    //       printf("You give 1 star rating");
+    //       break;
+    //     case 2:
+    //        printf("You give 2 star rating");
+    //        break;
+    //     case 3:
+    //        printf("You give 3 star rating");
+    //        break;   
+    //     case 4:
+    //        printf("You give 4 star rating");
+    //        break; 
+    //     case 5:
+    //         printf("You give 4 star rating");
+    //         break;
+    //     default:
+    //         printf("Thank you for your rating");
+    //         break;
+    // }
+
+   return 0;
+}
+
